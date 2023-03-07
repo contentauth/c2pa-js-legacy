d3a71a7a27f42e123df626f8701a4f8f24327cad
Massy Mahamli <mmahamli@adobe.com>
2023-03-07 14:07:09 -0500

    added icons & map

---
+++ packages/c2pa/src/lib/icon.ts
@@ -0,0 +1,33 @@
+import { default as animationVideo, default as unknown } from '../../assets/svg/alert-circle-dark.svg';
+import colorAdjustements from '../../assets/svg/color-palette-dark.svg';
+import cropped from '../../assets/svg/crop-dark.svg';
+import drawing from '../../assets/svg/draw-dark.svg';
+import edited from '../../assets/svg/edit-in-light-dark.svg';
+import opened from '../../assets/svg/import-dark.svg';
+import combinedAssets from '../../assets/svg/layers-dark.svg';
+import created from '../../assets/svg/new-item-dark.svg';
+import threed from '../../assets/svg/orbit-dark.svg';
+import filtered from '../../assets/svg/properties-dark.svg';
+import resized from '../../assets/svg/resize-dark.svg';
+import orientation from '../../assets/svg/rotate-left-outline-dark.svg';
+import placed from '../../assets/svg/save-to-light-dark.svg';
+import text from '../../assets/svg/text-dark.svg';
+
+export const icons  = {
+    'c2pa.unknown' : unknown,
+'c2pa.color_adjustments' : colorAdjustements,
+    'c2pa.cropped' : cropped, 
+    'c2pa.drawing': drawing,
+    'c2pa.edited': edited, 
+    'c2pa.opened': opened, 
+    'c2pa.created': created, 
+    'c2pa.filtered': filtered, 
+    'c2pa.resized': resized, 
+    'c2pa.orientation': orientation, 
+    'c2pa.placed': placed, 
+    'com.adobe.3d': threed,
+    'com.adobe.animation_video' : animationVideo, 
+    'com.adobe.combined_asssets' : combinedAssets, 
+    'com.adobe.text' :  text
+
+}
