6c9409ce283e0729aa9f2eff91f3a93929e26148
Dave Kozma <dkozma@adobe.com>
2023-03-23 10:01:03 -0400

    Remove console.log

--- packages/c2pa/rollup.config.js
+++ packages/c2pa/rollup.config.js
@@ -32,8 +32,6 @@ const banner = `
  **************************************************************************/
 `;
 
-console.log('integrity', integrity);
-
 const outputDir = resolve(__dirname, './dist');
 const developmentMode = process.env.ROLLUP_WATCH === 'true';
 const plugins = [
